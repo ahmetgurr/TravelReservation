054b5409fc4c9e0732660220cf1439478e20d83d
ahmetgurr <ahhmetgur@gmail.com>
2023-11-20 02:42:32 +0300

    Get data from firestore

--- app/src/main/java/com/example/travelreservation/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/travelreservation/ui/fragment/SettingFragment.kt
@@ -2,24 +2,35 @@ package com.example.travelreservation.ui.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.travelreservation.R
 import com.example.travelreservation.ui.login.LoginActivity
 import com.example.travelreservation.databinding.FragmentSettingBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class SettingFragment : Fragment() {
     private lateinit var binding: FragmentSettingBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+
+    private lateinit var txtUsername: TextView
+    private lateinit var txtEmail: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = FragmentSettingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+
         auth = Firebase.auth
+        db = FirebaseFirestore.getInstance()
         setHasOptionsMenu(true) // Bu, Fragment'ın kendi menüsünü kullanacağını belirtir
     }
 
@@ -27,13 +38,12 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentSettingBinding.inflate(inflater, container, false)
         return binding.root
-        //return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.signOutButton.setOnClickListener {
             auth.signOut()
@@ -42,10 +52,44 @@ class SettingFragment : Fragment() {
             activity?.finish()
         }
 
+       // auth = Firebase.auth
+        //db = FirebaseFirestore.getInstance()
+
+        // Kullanıcının UID'sini kontrol etme
+        val user = auth.currentUser
+        if (user != null) {
+            val userId = user.uid
+            Log.d("SettingFragment", "Current User ID: $userId")
+
+            // Firestore'dan kullanıcı adını çekme
+            loadUsernameFromFirestore(userId)
+        } else {
+            // Kullanıcı oturum açmamışsa, başka bir işlem yapabilirsiniz.
+            Log.d("SettingFragment", "User is not signed in.")
+        }
     }
 
+    private fun loadUsernameFromFirestore(userId: String) {
+        db.collection("Users")
+            .document(userId)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    // Firestore'dan kullanıcı adını al ve UI elemanına ata
+                    val username = document.getString("username")
+                    val email = document.getString("email")
 
-
+                    binding.txtUsername.text =  "Username: $username"
+                    binding.txtUsernamePhoto.text = "$username"
+                    binding.txtEmail.text =     "Email:         $email"
+                } else {
+                    Log.d("SettingFragment", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("SettingFragment", "get failed with ", exception)
+            }
+    }
 
 /*
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- app/src/main/java/com/example/travelreservation/ui/login/RegisterActivity.kt
+++ app/src/main/java/com/example/travelreservation/ui/login/RegisterActivity.kt
@@ -7,11 +7,13 @@ import android.widget.Toast
 import com.example.travelreservation.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +22,21 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = Firebase.auth
+        db = FirebaseFirestore.getInstance()
 
         binding.signUpClicked.setOnClickListener{
             val email = binding.signupEmail.text.toString()
             val password = binding.signupPassword.text.toString()
+            val username = binding.signupUsername.text.toString()
+
             val confirmPassword = binding.signupConfirm.text.toString()
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && username.isNotEmpty()){
                 if (password == confirmPassword){
                     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
                         if (it.isSuccessful){
+                            //kullanıcı adı kayıt ediliyor
+                            saveUserNameToFirestore()
+
                             val intent = Intent(this, LoginActivity::class.java)
                             startActivity(intent)
                         } else {
@@ -52,8 +60,26 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(loginIntent)
             finish()
         }
+    }
 
+    private fun saveUserNameToFirestore() {
+        val user = auth.currentUser
+        if (user != null) {
+            val userId = user.uid
+            val userHashMap = hashMapOf<String, Any>()
+            userHashMap["username"] = binding.signupUsername.text.toString()
+            userHashMap["email"] = binding.signupEmail.text.toString()
 
+            // Kullanıcının UID'sini kullanarak belgeyi oluştur
+            db.collection("Users").document(userId).set(userHashMap)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "User saved to firestore", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Failed to save user to firestore", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 /*
 // binding yerine onClick methodu kullanılırsa aşağıdaki işlmeleri kullan.
